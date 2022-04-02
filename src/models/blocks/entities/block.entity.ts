6286bddacd9c0c574e434e85b54c5496e2c0b493
Chanki Min <alscksrl2010@gmail.com>
2022-04-02 15:00:24 +0900

    Generate block, page, slide CRUD endpoints

---
+++ src/models/blocks/entities/block.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Tree,
+  TreeChildren,
+  TreeParent,
+  UpdateDateColumn,
+} from 'typeorm';
+import {
+  BlockProperty,
+  BlockTypes,
+} from '../interfaces/blockProperties.interface';
+
+@Entity()
+@Tree('closure-table')
+export class Block {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column()
+  type: BlockTypes; // title_1, image 등의 블록 타입을 지정
+
+  @Column({ type: 'json' })
+  properties: BlockProperty; // type에 따른 블록의 데이터
+
+  @TreeChildren()
+  children: Block[]; // 문제점: Typeorm 에서 children의 순서를 지정하기 어려움
+
+  @Column({ type: 'int' })
+  order: number;
+
+  @TreeParent()
+  parent: Block;
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+}
