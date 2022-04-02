935b4f9b6861d78a64cb22298182a99bf29e81d0
Chanki Min <alscksrl2010@gmail.com>
2022-04-02 15:10:41 +0900

    Add interface type guards

--- src/models/blocks/interfaces/blockProperties.interface.ts
+++ src/models/blocks/interfaces/blockProperties.interface.ts
@@ -4,12 +4,12 @@ export type BlockProperty =
   | to_do_list_props
   | bulleted_list_props
   | numbered_list_props
-  | media_image
-  | media_video
-  | media_file
+  | media_image_props
+  | media_video_props
+  | media_file_props
   | carousel_slide
-  | simple_margin
-  | grid;
+  | simple_margin_props
+  | grid_props;
 
 export type BlockTypes =
   | 'plain_text'
@@ -30,8 +30,16 @@ interface plain_text_props {
   text: string; // html strings, should be sanitized
 }
 
+export function isPlainTextProps(u: any): u is plain_text_props {
+  return typeof u?.text === 'string';
+}
+
 interface heading_props {
   text: string; // html strings, should be sanitized
+}
+
+export function isHeadingProps(u: any): u is heading_props {
+  return typeof u?.text === 'string';
 }
 
 interface to_do_list_props {
@@ -39,30 +47,66 @@ interface to_do_list_props {
   text: string; // html strings, should be sanitized
 }
 
+export function isToDoListProps(u: any): u is to_do_list_props {
+  return typeof u?.text === 'string' && typeof u?.checked === 'boolean';
+}
+
 interface bulleted_list_props {
   text: string; // html strings, should be sanitized
+}
+
+export function isBulltedListProps(u: any): u is bulleted_list_props {
+  return typeof u?.text === 'string';
 }
 
 interface numbered_list_props {
   text: string; // html strings, should be sanitized
 }
 
-interface media_image {
+export function isNumberdListProps(u: any): u is numbered_list_props {
+  return typeof u?.text === 'string';
+}
+
+interface media_image_props {
   width: number;
   height: number;
   src: string; // URL 주소
 }
 
-interface media_video {
+export function isMediaImageProps(u: any): u is media_image_props {
+  return (
+    typeof u?.src === 'string' &&
+    typeof u?.width === 'number' &&
+    typeof u?.height === 'number'
+  );
+}
+
+interface media_video_props {
   width: number;
   height: number;
   src: string; // URL 주소
 }
 
-interface media_file {
+export function isMediaVideoProps(u: any): u is media_video_props {
+  return (
+    typeof u?.src === 'string' &&
+    typeof u?.width === 'number' &&
+    typeof u?.height === 'number'
+  );
+}
+
+interface media_file_props {
   title: string;
   size: number; // 파일 사이즈
   src: string; // URL 주소
+}
+
+export function isMediaFileProps(u: any): u is media_file_props {
+  return (
+    typeof u?.title === 'string' &&
+    typeof u?.size === 'number' &&
+    typeof u?.src === 'string'
+  );
 }
 
 interface carousel_slide {
@@ -70,11 +114,22 @@ interface carousel_slide {
   enalble_navigation_buttons: boolean;
 }
 
-interface simple_margin {
+export function isCarouselSlideProps(u: any): u is carousel_slide {
+  return (
+    typeof u?.enable_index_numbers === 'boolean' &&
+    typeof u?.enalble_navigation_buttons === 'boolean'
+  );
+}
+
+interface simple_margin_props {
   height: number;
 }
 
-interface grid {
+export function isSimpleMargin(u: any): u is simple_margin_props {
+  return typeof u?.height === 'number';
+}
+
+interface grid_props {
   row_size: number;
   col_size: number;
 
@@ -83,4 +138,15 @@ interface grid {
 
   width: number;
   height: number;
+}
+
+export function isGridProps(u: any): u is grid_props {
+  return (
+    typeof u?.row_size === 'number' &&
+    typeof u?.col_size === 'number' &&
+    typeof u?.width === 'number' &&
+    typeof u?.height === 'number' &&
+    Array.isArray(u?.row_ratios) &&
+    Array.isArray(u?.col_ratios)
+  );
 }
